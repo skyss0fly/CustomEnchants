d41ea0dd3fe366d8f99856503d92244e15b0754a
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-07-15 17:39:54 +1000

    Update BaseEnchants.php

--- src/skyss0fly/CustomEnchants/enchants/BaseEnchants.php
+++ src/skyss0fly/CustomEnchants/enchants/BaseEnchants.php
@@ -7,6 +7,17 @@ class BaseEnchants {
 const $lifesteal,
 const $driller,
 const $molotov,
-  
+
+  private enchants() {
+$legendary = 100;
+$godly = 95;
+$mythical = 90;
+$mid = 50;
+$common = 25;
+$awful = 10;
+$horrible = 1;
+  $rarities = "$lifesteal 25, $driller 90, $molotov 90";
+}
+
 
 }
